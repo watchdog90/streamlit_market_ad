18e087a851994a2eb809649093f4e9b55f2e002d
TENGXIANG SU <33008779+watchdog90@users.noreply.github.com>
2023-06-13 19:48:01 +0100

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 st.set_option('deprecation.showPyplotGlobalUse', False)
 import pickle
 import warnings
